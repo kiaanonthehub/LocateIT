fc239c71d4b3dd6b027c3806775bc87a02d7053e
arxhary <tyrese010826@gmail.com>
2022-10-08 13:29:48 +0200

    Initial commit

---
+++ app/src/main/java/com/locateitteam/locateit/MainActivity.java
@@ -0,0 +1,31 @@
+package com.locateitteam.locateit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.btnlogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
