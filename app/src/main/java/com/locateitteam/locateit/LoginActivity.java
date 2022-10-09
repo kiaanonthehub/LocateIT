cbba401c8c47406be80cab25f581d4fca6cccc0f
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-10-09 13:29:49 +0200

    updated the login class and ui

--- app/src/main/java/com/locateitteam/locateit/LoginActivity.java
+++ app/src/main/java/com/locateitteam/locateit/LoginActivity.java
@@ -26,39 +26,35 @@ public class LoginActivity extends AppCompatActivity {
 
         LoginButton = findViewById(R.id.btnLogin);
 
-    LoginButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
-            startActivity(intent);
-        }
-    });
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
 
-    private void init()
-    {
+    private void init() {
         //
     }
 
     // check if the user has the correct google play services version installed
-    public boolean isServicesOK()
-    {
+    public boolean isServicesOK() {
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(LoginActivity.this);
 
-        if(available == ConnectionResult.SUCCESS)
-        {
+        if (available == ConnectionResult.SUCCESS) {
             // the version is up to date and the user can make map requests
-            Log.d(TAG,"isServicesOK: Google Play Services is working");
-             return true;
-        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
-        {
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             // an error occured but can be resolved
-            Log.d(TAG,"isServicesOK: an error occured but can be resolved");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(LoginActivity.this,available,ERROR_DIALOG_REQUEST);
+            Log.d(TAG, "isServicesOK: an error occured but can be resolved");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(LoginActivity.this, available, ERROR_DIALOG_REQUEST);
             dialog.show();
-        }else{
+        } else {
             Toast.makeText(this, "Map request cannot be performed", Toast.LENGTH_SHORT).show();
         }
         return false;
