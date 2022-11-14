5a01d08ddaa4598c3e897612a2f308084e15fc0d
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-11-14 16:43:19 +0200

    Added activity notes and layout resource file

---
+++ app/src/main/java/com/locateitteam/locateit/Activity/NotesActivity.java
@@ -0,0 +1,16 @@
+package com.locateitteam.locateit.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.locateitteam.locateit.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
