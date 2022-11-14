d045b4b62fa71bbb20802b97fe45be210b38b127
Tyler Jagdew <jagdewtyler@gmail.com>
2022-11-14 20:29:57 +0200

    Error with Firestore

--- app/src/main/java/com/locateitteam/locateit/Activity/NotesActivity.java
+++ app/src/main/java/com/locateitteam/locateit/Activity/NotesActivity.java
@@ -2,6 +2,8 @@ package com.locateitteam.locateit.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +12,25 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.locateitteam.locateit.R;
 
 public class NotesActivity extends AppCompatActivity {
 
     // declare component
     FloatingActionButton notesFab;
+    private FirebaseAuth firebaseAuth;
+
+    RecyclerView mrecyclerview;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+
+    FirestoreRecyclerAdapter<firebasemodel,NoteViewHolder> noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,10 @@ public class NotesActivity extends AppCompatActivity {
 
         // initialise component
         notesFab = findViewById(R.id.noteFab);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore=FirebaseFirestore.getInstance();
 
         getSupportActionBar().setTitle("All Notes");
 
@@ -31,19 +50,23 @@ public class NotesActivity extends AppCompatActivity {
         notesFab.setOnClickListener(view -> {
             startActivity(new Intent(NotesActivity.this,CreateNotesActivity.class));
         });
+
+        Query query=firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").orderBy("Title",Query.Direction.ASCENDING);
+    FirestoreRecyclerOptions<firebasemodel> allusernotes= FirestoreRecyclerOptions.Builder<firebasemodel>().setQuery(query,firebasemodel.class).build();
+    // time stamp @ 11:17 on PART 10
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu,menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//
-//
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu,menu);
+      return true;
+   }
+
+    @Override
+   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+
+       return super.onOptionsItemSelected(item);
+    }
 }
